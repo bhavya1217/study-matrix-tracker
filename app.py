922e82214431a294680b0078234475119879e3a0
bhavya <bhavyatripathi0@gmail.com>
2026-07-22 14:41:46 +0530

    Fix port for Render cloud deployment

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
+import os
 from flask import Flask
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super_secret_study_key'
-# CORS allows your frontend HTML to talk to this Python backend securely
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 @socketio.on('sync_matrix')
 def handle_matrix_sync(data):
     emit('partner_matrix_updated', data, broadcast=True, include_self=False)
 
-
 @socketio.on('send_xp_boost')
 def handle_boost(data):
     emit('receive_xp_boost', data, broadcast=True, include_self=False)
 
 if __name__ == '__main__':
-    print("🚀 Study Buddy Server running on http://127.0.0.1:5000")
-    socketio.run(app, debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
